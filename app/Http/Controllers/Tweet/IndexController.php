6dde839915b9ada837b298f4899c37123132eabf
mitani1993 <fytim55@gmail.com>
2022-06-13 20:50:39 +0900

    17 $nameの表示

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -15,6 +15,6 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return 'Shingle Action!';
+        return view('tweet.index', ['name' => 'laravel']);
     }
 }
