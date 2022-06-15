01332fed57cf0257536462cc1ac812e4eaad29c2
mitani1993 <fytim55@gmail.com>
2022-06-15 19:54:16 +0900

    編集用の投稿画面の作成

--- app/Http/Controllers/Tweet/Update/IndexController.php
+++ app/Http/Controllers/Tweet/Update/IndexController.php
@@ -9,17 +9,12 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class IndexController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function __invoke(Request $request)
     {
         $tweetId = (int) $request->route('tweetId');
         $tweet = Tweet::where('id', $tweetId)->firstOrFail();
 
-        dd($tweet);
+        return view('tweet.update')
+            ->with('tweet', $tweet);
     }
 }
