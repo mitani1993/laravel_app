8da32bee76c9a5ed2e70453f035691d62d142346
mitani1993 <fytim55@gmail.com>
2022-06-20 21:57:27 +0900

    フラッシュメッセージ追加

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -1,8 +1,8 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scakable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>つぶやきアプリ</title>
 </head>
@@ -11,6 +11,9 @@
     @auth
         <div>
             <p>投稿フォーム</p>
+            @if (session('feedback.success'))
+                <p style="color: green">{{ session('feedback.success') }}</p>
+            @endif
             <form action="{{ route('tweet.create') }}" method="post">
                 @csrf
                 <label for="tweet-content">つぶやき</label>
@@ -24,18 +27,18 @@
         </div>
     @endauth
     <div>
-        @foreach ($tweets as $tweet)
+        @foreach($tweets as $tweet)
             <details>
                 <summary>{{ $tweet->content }} by {{ $tweet->user->name }}</summary>
                 @if (\Illuminate\Support\Facades\Auth::id() === $tweet->user_id)
                     <div>
                         <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
+                        <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
+                            @method('DELETE')
+                            @csrf
+                            <button type="submit">削除</button>
+                        </form>
                     </div>
-                    <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
-                        @method('DELETE')
-                        @csrf
-                        <button type="submit">削除</button>
-                    </form>
                 @else
                     編集できません
                 @endif
