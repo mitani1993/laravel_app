50a872a6f83bc397c2c05590d9bc5a31ec7e690a
mitani1993 <fytim55@gmail.com>
2022-06-21 09:06:13 +0900

    キャッシュバスティング

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>つぶやきアプリ</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    <script src="{{ mix('/js/app.js') }}"></script>
 </head>
 <body>
     <h1>つぶやきアプリ</h1>
