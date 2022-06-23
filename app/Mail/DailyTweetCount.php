0732cb6890503bfe9dc4026bc8be18e3478b6bd7
mitani1993 <fytim55@gmail.com>
2022-06-23 22:10:13 +0900

    スケジュールに登録する

--- app/Mail/DailyTweetCount.php
+++ app/Mail/DailyTweetCount.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class DailyTweetCount extends Mailable
+class DailyTweetCount extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
