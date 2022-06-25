c4981c9523c0318391c2c39b52945dea812d56f6
mitani1993 <fytim55@gmail.com>
2022-06-25 20:34:22 +0900

    モックを利用する

--- tests/Unit/Service/TweetServiceTest.php
+++ tests/Unit/Service/TweetServiceTest.php
@@ -3,16 +3,31 @@
 namespace Tests\Unit\Service;
 
 use PHPUnit\Framework\TestCase;
+use App\Services\TweetService;
+use Mockery;
 
 class TweetServiceTest extends TestCase
 {
     /**
-     * A basic unit test example.
+     * A basic unit test check_own_tweet.
      *
+     * @runInSeparateProcess
      * @return void
      */
-    public function test_example()
+    public function test_check_own_tweet()
     {
-        $this->assertTrue(true);
+        $tweetService = new TweetService();
+
+        $mock = Mockery::mock('alias:App\Models\Tweet');
+        $mock->shouldReceive('where->first')->andReturn((object)[
+            'id' => 1,
+            'user_id' => 1
+        ]);
+
+        $result = $tweetService->checkOwnTweet(1, 1);
+        $this->assertTrue($result);
+
+        $result = $tweetService->checkOwnTweet(2, 1);
+        $this->assertFalse($result);
     }
 }
